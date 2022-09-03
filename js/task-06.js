b712c0d1adfd7a4aa30728c7124dfd3c5b6ce8c3
Serge-Moskalenko <skmos2019@outlook.com>
2022-09-03 12:08:55 +0100

    fix the end

--- js/task-06.js
+++ js/task-06.js
@@ -7,9 +7,7 @@ refs.inputEl.addEventListener('blur', eventOnImput);
 function eventOnImput(event) {
   const ev = event.currentTarget;
 
-  if (String(ev.value.length) === refs.inputEl.dataset.length) {
-      console.log(ev.value.length)
-      return  ev.classList.add('valid');
-    }
-  return ev.classList.add('invalid');
+  return String(ev.value.length) === refs.inputEl.dataset.length
+    ? ev.classList.add('valid')
+    : ev.classList.add('invalid');
 };
